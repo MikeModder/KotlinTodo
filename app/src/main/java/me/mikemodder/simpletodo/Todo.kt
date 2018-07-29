37ab27a806695014dde430ae2ac5ab5eaa428f19
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-07-28 20:48:23 -0400

    Add new build flavor                                                          Add code to regenerate the ListView                                                          Add proper ID things to the Todos                                                          Add todo deletion (from DB too) by long pressing the item

--- app/src/main/java/me/mikemodder/simpletodo/Todo.kt
+++ app/src/main/java/me/mikemodder/simpletodo/Todo.kt
@@ -1,6 +1,7 @@
 package me.mikemodder.simpletodo
 
-class Todo(todoText: String, done: Boolean = false) {
+class Todo(todoText: String, done: Boolean = false, id: Int = -1) {
     var text: String = todoText
     var done: Boolean = done
+    var id: Int = id
 }
